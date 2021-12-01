2146b9c2a3e3e52d27b9e49cf464a25b26ab4ed1
Frieder Erdmann <frieder.erdmann@massive.se>
2021-12-01 11:18:12 +0100

    reduced to unreadable one liners for good measure

--- day_01.py
+++ day_01.py
@@ -1,36 +1,27 @@
 # from utils import file_reader
 from typing import List
+from itertools import pairwise
 
 
-def part_one(depth_meassures: List[int]) -> int:
-    increase = 0
-
-    for i, meassure in enumerate(depth_meassures):
-        if not i:
-            continue
-        prev = depth_meassures[i - 1]
-        if meassure > prev:
-            increase += 1
-
-    return increase
+def part_one(meassures: List[int]) -> int:
+    return sum([i < j for i, j in pairwise(meassures)])
 
 
-def part_two(depth_meassures: List[int], window_size: int = 3) -> int:
-    averaged = list()
-    for i in range(len(depth_meassures)):
-        if i + window_size <= len(depth_meassures):
-            averaged.append(sum(depth_meassures[i:i + window_size]))
+def part_two(meassures: List[int], window: int = 3) -> int:
+    return part_one([sum(meassures[i : i + window]) for i in range(len(meassures))])
 
-    increase = part_one(averaged)
 
-    return increase
+def main():
+    file_path = "inputs/day_01.txt"
 
-file_path = "inputs/day_01.txt"
+    input_list: List[int]
 
-input_list: List[int]
+    with open(file_path, 'r') as file_handler:
+        input_list = [int(i) for i in file_handler.readlines()]
 
-with open(file_path, 'r') as file_handler:
-    input_list = [int(i) for i in file_handler.readlines()]
+    print(part_one(input_list))
+    print(part_two(input_list, 3))
 
-print(part_one(input_list))
-print(part_two(input_list))
+
+if __name__ == "__main__":
+    main()
