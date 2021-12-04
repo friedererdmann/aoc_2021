fbf3e09549d702b95d1a799404238ca61be4f5ae
friedererdmann <55715326+friedererdmann@users.noreply.github.com>
2021-12-04 09:37:53 +0100

    fixing spacing in reader

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -1,8 +1,10 @@
 from typing import List
 
+
 def read_file_to_lines(input_path: str) -> List[str]:
     with open(input_path, 'r') as file_handler:
         return [x.rstrip() for x in file_handler.readlines()]
+
 
 def get_list_of_ints_from_file(input_path: str) -> List[int]:
     return [int(i) for i in read_file_to_lines(input_path)]
