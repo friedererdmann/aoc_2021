721f5327d06112126e1bb3c63b11fb6e51ea7d01
friedererdmann <55715326+friedererdmann@users.noreply.github.com>
2021-12-07 07:52:50 +0100

    shortened

--- day_07.py
+++ day_07.py
@@ -4,36 +4,27 @@ from utils.file_reader import read_file_to_lines
 
 def data_prep(instructions):
     data = [int(x) for x in instructions[0].split(",")]
-    # print(sum(data), len(data))
     return data
 
 
-def part_one(instructions):
+def part_one(data):
     minimum = sys.maxsize
-    point = 0
-    for i in range(max(instructions)):
-        distance = sum([abs(x - i) for x in instructions])
-        if distance < minimum:
-            minimum = distance
-            point = i
-    return point, minimum
+    for i in range(max(data)):
+        minimum = min(minimum, sum([abs(x - i) for x in data]))
+    return minimum
 
 
-def part_two(instructions):
+def part_two(data):
     minimum = sys.maxsize
-    point = 0
-    for i in range(max(instructions)):
-        distance = sum([(((x - i) ** 2) + abs(x - i)) / 2 for x in instructions])
-        if distance < minimum:
-            minimum = distance
-            point = i
-    return point, minimum
+    for i in range(max(data)):
+        minimum = min(minimum, sum([(((x - i) ** 2) + abs(x - i)) // 2 for x in data]))
+    return minimum
 
 
 def main():
     file_path = "inputs/day_07.txt"
     instructions = read_file_to_lines(file_path)
-    instructions = ["16,1,2,0,4,2,7,1,2,14"]
+    # instructions = ["16,1,2,0,4,2,7,1,2,14"]
     data = data_prep(instructions)
     print(part_one(data))
     print(part_two(data))
